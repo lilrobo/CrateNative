fdff4c1a30f34d4dd5d6b0dd9620e69f86684f2d
smaarmstrong <seanmaarmstrong@gmail.com>
2024-11-07 12:15:29 +0000

    linting

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import {Text, View} from "react-native"
 
 export default function Index() {
   return (
@@ -7,9 +7,8 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
+      }}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
-  );
+  )
 }
